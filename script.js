2d902ffc16b9ebfbe7e4d52d7844e4d648617c9c
MLRiida <mellala40@gmail.com>
2025-07-29 00:09:57 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const counters = document.querySelectorAll(".counter");
 const animationSpeed = 500;
 
-      const updateCounter = (counter) => {
-            const targetCount = +counter.getAttribute("data-target");
-            let currentCount = +counter.innerText;
-            const animationStep = targetCount / animationSpeed;
+const updateCounter = (counter) => {
+    const targetCount = +counter.getAttribute("data-target");
+    let currentCount = +counter.innerText;
+    const animationStep = targetCount / animationSpeed;
 
-            if(currentCount < targetCount){
-                counter.classList.add('updating');
-                counter.innerText = Math.floor(currentCount + animationStep);
-                
-                setTimeout(() => {
-                    updateCounter(counter);
-                }, 1); 
-            } else {
-                counter.classList.remove('updating');
-                counter.innerText = targetCount;
-            }
-        }
+    if(currentCount < targetCount){
+        counter.classList.add('updating');
+        counter.innerText = Math.floor(currentCount + animationStep);
+        
+        setTimeout(() => {
+            updateCounter(counter);
+        }, 1); 
+    } else {
+        counter.classList.remove('updating');
+        counter.innerText = targetCount;
+    }
+}
 
-        // Start animation when page loads
-        window.addEventListener('load', () => {
-            counters.forEach((counter) => {
-                updateCounter(counter);
-            });
-        });
+// Start animation when page loads
+window.addEventListener('load', () => {
+    counters.forEach((counter) => {
+        updateCounter(counter);
+    });
+});
